393caf9cb7d6777907d500d12988e11746da33ff
SunnyBandre <bandresunny89@gmail.com>
2025-12-23 00:18:57 +0530

    changes

---
+++ components/home-page/hero.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/img.jpeg"
+          alt="An image showing Max"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Sunny</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ components/layout/logo.js
@@ -0,0 +1,7 @@
+import classes from "./logo.module.css";
+
+function Logo() {
+  return <div className={classes.logo}>Sunny' Next Blog</div>;
+}
+
+export default Logo;
